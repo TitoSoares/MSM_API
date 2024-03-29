28d98c0b63669c8d20d93f6c2e9cf6096fcccf75
Kristopher <kristopher.scsantos@senacsp.edu.br>
2024-03-28 22:27:53 -0300

    alterando o modulo de usuario para banco

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 import { Body, Delete, Param, Put } from "@nestjs/common";
 import { CriaUsuariosDTO } from "./dto/Cria-Usuario.dto";
 import { UsuariosEntity } from "./usuario.entity";
@@ -7,77 +7,50 @@ import { UsuariosArmazenados } from "./usuarios.dm";
 import { AlteraUsuarioDTO } from "./dto/Atualiza-Usuario.dto";
 import { LoginUsuarioDTO } from "./dto/Login-Usuario.dto";
 import { ApiResponse, ApiResponseProperty, ApiTags } from "@nestjs/swagger";
+import { UsuarioService } from "./usuario.service";
+import { RetornoCadastroDTO, RetornoObjDTO } from "src/dto/retorno.dto";
 
 @Controller('/usuarios')
 @ApiTags('usuarios')
 export class UsuariosController{
-    constructor(private claUsuariosArmazenados:UsuariosArmazenados){}
+    constructor(private readonly usuarioService: UsuarioService){}
 
     // Cria Usuario
     // ====================================== //// ====================================== //// ====================================== //
-        // ====Swagger============================ //
         @ApiResponse({status:200, description:"A operação foi um sucesso"})
         @ApiResponse({status:500, description:"Ocorreu um erro na operação! Tente novamente mais tarde"})
-        // ====Swagger============================ //
-    
+
         @Post()
-
-        async criaUsuario(@Body() dadosUsuarios:CriaUsuariosDTO){
-
-            var usuarios=new UsuariosEntity(uuid(),dadosUsuarios.nome,dadosUsuarios.email,dadosUsuarios.cpf,dadosUsuarios.cnpj,
-            dadosUsuarios.senha)
-
-            this.claUsuariosArmazenados.AdicionarUsuarios(usuarios)
-            var retorno={
-                id:usuarios.id,
-                message:"Usuario Criado"
-            }
-            return retorno
+        async criaUsuario(@Body() dados:CriaUsuariosDTO): Promise<RetornoCadastroDTO>{        
+            return this.usuarioService.inserir(dados)        
         }
+        
     // ====================================== //// ====================================== //// ====================================== //
 
 
     // Atualiza Usuario
     // ====================================== //// ====================================== //// ====================================== //
-        // ====Swagger============================ //
         @ApiResponse({status:200, description:"A operação foi um sucesso"})
         @ApiResponse({status:500, description:"Ocorreu um erro na operação! Tente novamente mais tarde"})
-        // ====Swagger============================ //
 
-        @Put("/:id")
 
-        async atualizaUsuario(@Param('id') id:string,@Body() novosDados:AlteraUsuarioDTO){
-
-            const usuarioAtualizado=await this.claUsuariosArmazenados.atualizaUsuario(id, novosDados)
-
-            return{
-                usuario:usuarioAtualizado,
-                message:"Usuário atulizado"
-            }
-
+        @Put(':id')
+        async alterarUsuario(@Body() dados: AlteraUsuarioDTO,@Param('id') id: string): Promise<RetornoCadastroDTO>{        
+            return this.usuarioService.alterar(id,dados)        
         }
-    // ====================================== //// ====================================== //// ====================================== //
 
-    
-    // Remove Usuarios
     // ====================================== //// ====================================== //// ====================================== //
-        // ====Swagger============================ //
+   
+
+    // Deleta Usuario 
+    // ====================================== //// ====================================== //// ====================================== //
         @ApiResponse({status:200, description:"A operação foi um sucesso"})
         @ApiResponse({status:500, description:"Ocorreu um erro na operação! Tente novamente mais tarde"})
-        // ====Swagger============================ //
-
-        @Delete('/:id')
-
-        async removeUsuario(@Param("id")id:string){
-
-            const usuarioRemovido=await this.claUsuariosArmazenados.removeUsuario(id)
-
-            return{
-                usuario:usuarioRemovido,
-                message:"Usuário removido"
-            }
-
-        }
+        
+        @Delete('remove-:id')
+        async removeGenero(@Param('id') id: string): Promise<RetornoObjDTO>{
+            return this.usuarioService.remover(id);
+        }    
 
     // ====================================== //// ====================================== //// ====================================== //  
     
@@ -91,16 +64,10 @@ export class UsuariosController{
 
         @Post('/login')
 
-        async Login(@Body() dadosUsuario:LoginUsuarioDTO){
+        async login(@Body() dados: LoginUsuarioDTO): Promise<RetornoObjDTO>{
+            return this.usuarioService.login(dados.EMAIL,dados.SENHA);
+        }    
 
-            var login = this.claUsuariosArmazenados.validarLogin(dadosUsuario.email,dadosUsuario.senha)
-
-            
-                return{
-                    usuario: login[1] ? login[0] : null,
-                    status: login[1],
-                    message: login[1] ? "Login Efetuado" : "Usuario ou senha invalidos !"
-                }
         }
     // ====================================== //// ====================================== //// ====================================== // 
 }
