58f6322a1365bc4cf2379a45733d4e82a7b2d2bd
Kristopher <kristopher.scsantos@senacsp.edu.br>
2024-04-11 22:04:41 -0300

    .

--- src/entrada_saida/entrada_saida.module.ts
+++ src/entrada_saida/entrada_saida.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { DatabaseModule } from "src/database/database.module";
 import { UsuarioProviders } from "src/usuarios/usuario.providers";
 import { UsuarioService } from "src/usuarios/usuario.service";
 import { Entrada_saidaController } from "./entrada_saida.controller";
@@ -8,6 +9,7 @@ import { Entrada_saidaService } from "./entrada_saida.service";
 
 @Module({
 
+    imports: [DatabaseModule],
     controllers:[Entrada_saidaController],
     providers:[    ...UsuarioProviders,
         UsuarioService,
